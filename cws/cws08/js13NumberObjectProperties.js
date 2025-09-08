b513ce5199bb2bc118b9e36848f9d5f7ca955fc0
MGilmartin <merrua@gmail.com>
2025-09-08 20:23:38 +0100

    what num is it

---
+++ cws/cws08/js13NumberObjectProperties.js
@@ -0,0 +1,52 @@
+// Task
+
+// Coding in function whatNumberIsIt.
+//  function accept 1 parameter:n. it's a number.
+
+// To judge the number n.
+// If n is one of the above five constants,
+// return one of these string:
+// "Input number is Number.MAX_VALUE"
+// "Input number is Number.MIN_VALUE"
+// "Input number is Number.NaN"
+// "Input number is Number.NEGATIVE_INFINITY"
+// "Input number is Number.POSITIVE_INFINITY"
+
+// Other values should return
+// "Input number is xxx". xxx means this number.
+
+// For example:
+// whatNumberIsIt(1/0) should return "Input number is Number.POSITIVE_INFINITY"
+// whatNumberIsIt(100) should return "Input number is 100"
+
+// What you need to think about is
+// how to judge it correctly
+// and effectively
+// and don't forget isNaN().
+
+function whatNumberIsIt(n) {
+  if (isNaN(n)) {
+    return `Input number is Number.NaN`;
+  }
+  if (n === Number.MAX_VALUE) {
+    return `Input number is Number.MAX_VALUE`;
+  }
+  if (n === Number.MIN_VALUE) {
+    return `Input number is Number.MIN_VALUE`;
+  }
+  if (n === Number.NEGATIVE_INFINITY) {
+    return `Input number is Number.NEGATIVE_INFINITY`;
+  }
+  if (n === Number.POSITIVE_INFINITY) {
+    return `Input number is Number.POSITIVE_INFINITY`;
+  }
+  return `Input number is ${n}`;
+}
+
+console.log(whatNumberIsIt(1 / 0));
+console.log(whatNumberIsIt(100));
+console.log(whatNumberIsIt(1.7976931348623157e308));
+console.log(whatNumberIsIt(5e-324));
+console.log(whatNumberIsIt(-Number.MAX_VALUE * 2));
+console.log(whatNumberIsIt(NaN));
+console.log(whatNumberIsIt(Infinity + 1));

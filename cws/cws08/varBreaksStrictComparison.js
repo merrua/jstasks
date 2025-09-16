0f9c1d930e91c69cab866832cf227e4052b0adc7
MGilmartin <merrua@gmail.com>
2025-09-16 22:50:02 +0100

    var breaks strict comparison

---
+++ cws/cws08/varBreaksStrictComparison.js
@@ -0,0 +1,16 @@
+// Description:
+
+// In JavaScript, there is a special case where strict
+// comparison of the same variable returns false!
+// Try to find out what must be done to get such result!
+// var x = something;
+// x === x // returns false!
+// Write a function which will return
+// value for which strict comparison will give false!
+
+function findStrangeValue() {
+  var x = NaN;
+  return x === x;
+}
+
+console.log(findStrangeValue());

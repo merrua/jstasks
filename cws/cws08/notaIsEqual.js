c2c7846ff8d18a9a11dcd6ba0398695fd0ebf9b6
MGilmartin <merrua@gmail.com>
2025-10-14 18:30:38 +0100

    not a kata

---
+++ cws/cws08/notaIsEqual.js
@@ -0,0 +1,12 @@
+// You task to pass only this tests :
+
+//     a == false
+//     !a == false
+//     a == !a
+
+// const a = new Boolean(); //(also)
+// const a = [] //(also)
+const a = "0";
+console.log(a == false);
+console.log(!a == false);
+console.log(a == !a);

950d2ebe724e326e1558acdc51f9e9c9eb0a2e3d
MGilmartin <merrua@gmail.com>
2025-12-09 16:48:42 +0000

    total pressure

--- cws/cws08/totalPressure.js
+++ cws/cws08/totalPressure.js
@@ -1,11 +1,21 @@
+// solution = (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
+//   /* ptotal = (m1/M1 + m2/M2)RT/V */
+//   let m_t = givenMass1 / molarMass1 + givenMass2 / molarMass2;
+//   let T_kelvin = temp + 273.15;
+//   let R = 0.082;
+//   let top = m_t * R * T_kelvin;
+//   let total = top / volume;
+//   return total;
+// };
+
 solution = (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
   /* ptotal = (m1/M1 + m2/M2)RT/V */
-  let m_t = givenMass1 / molarMass1 + givenMass2 / molarMass2;
-  let T_kelvin = temp + 273.15;
-  let R = 0.082;
-  let top = m_t * R * T_kelvin;
-  let total = top / volume;
-  return total;
+  return (
+    ((givenMass1 / molarMass1 + givenMass2 / molarMass2) *
+      0.082 *
+      (temp + 273.15)) /
+    volume
+  );
 };
 
-console.log(solution(44, 30, 3, 2, 5, 50));
+console.log(solution(44, 30, 3, 2, 5, 50)); //0.714651121212121
